4cf0f17d39c11ec583e5e59ca389f56b8648b565
TEbenhoech <tebenhoehc@csumb.edu>
2017-10-21 17:49:44 -0700

    Prototype

---
+++ Assets/Behavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NPBehave;
+
+public class Behavior : MonoBehaviour
+{
+    public Material lecturing;
+    public Material questions;
+    private Root behavior;
+    public List<StudentBehavior> studentQuestions;
+
+    private void CheckLecturing()
+    {
+        if (behavior.Blackboard.Get<bool>("lecture"))
+        {
+            behavior.Blackboard["lecture"] = Time.time < (behavior.Blackboard.Get<float>("time") + 2f);
+        }
+        else
+        {
+            behavior.Blackboard["lecture"] = studentQuestions.Count == 0;
+        }
+    }
+
+    private void answerQuestion()
+    {
+        if (studentQuestions.Count == 0)
+            return;
+        switch (studentQuestions[0].question)
+        {
+            case 0:
+                studentQuestions.RemoveAt(0);
+                behavior.Blackboard["time"] = Time.time;
+                break;
+            case 1:
+                if(Time.time > behavior.Blackboard.Get<float>("time") + studentQuestions[0].time)
+                {
+                    studentQuestions[0].question = 0;
+                }
+                break;
+            case 2:
+                studentQuestions[0].question = 1;
+                behavior.Blackboard["time"] = Time.time;
+                break;
+        }
+    }
+
+    void Start()
+    {
+        behavior = CreateBehaviorTree();
+        behavior.Start();
+#if UNITY_EDITOR
+        Debugger debugger = (Debugger)this.gameObject.AddComponent(typeof(Debugger));
+        debugger.BehaviorTree = behavior;
+#endif
+    }
+
+    private Root CreateBehaviorTree()
+    {
+        return new Root(
+            new Service(0.5f, () => CheckLecturing(),
+            new Selector(
+                new BlackboardCondition("lecture", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+                    new Sequence(new Action(() => gameObject.GetComponent<Renderer>().material = lecturing), new WaitUntilStopped())),
+                new BlackboardCondition("lecture", Operator.IS_EQUAL, false, Stops.IMMEDIATE_RESTART,
+                    new Sequence(new Action(() => { gameObject.GetComponent<Renderer>().material = questions; answerQuestion();}))))));
+    }
+
+}
---
+++ Assets/StudentBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentBehavior : MonoBehaviour {
+
+    public int question;
+    public float time;
+
+    public Material confused;
+    public Material gettingAnswered;
+    public Material understand;
+
+    private float questionTime;
+
+    private void checkForQuestion() {
+        if (Random.Range(0, 100) > 90) {
+            question = 2;
+            time = Random.Range(0f, 1f);
+            GameObject.Find("Professor").GetComponent<Behavior>().studentQuestions.Add(this);
+        }
+        questionTime = Time.time;
+    }
+	
+    void Start () {
+        questionTime = Time.time;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > questionTime + 1f)
+            checkForQuestion();
+        switch (question)
+        {
+            case 0:
+                gameObject.GetComponent<Renderer>().material = understand;
+                break;
+            case 1:
+                gameObject.GetComponent<Renderer>().material = gettingAnswered;
+                break;
+            case 2:
+                gameObject.GetComponent<Renderer>().material = confused;
+                break;
+        }
+	}
+}
